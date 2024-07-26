9920511095e20a23c5f2c850499c7cdb2a72dab2
Parisa Daily <misssweetardor@yahoo.com>
2024-07-25 18:31:01 -0700

    refactored my JS and removed a bunch of duplicate code. While I got my input data to store, the form is not clearing ugh. I am making progress, yay!

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,65 +1,28 @@
-const username = document.getElementById('username');
-const title = document.getElementById('title');
-const content = document.getElementById('content');
-const uponSubmit = document.getElementById('submit');
 
-//need to store these  objects 
+const submitEl = document.querySelector('form');
 
-const blogForm = {
-    username: username,
-    title: title,
-    content: content
-};
-
-
-//If  no data is entered form should return prompt user to compplete required fields
-//create a light or dark mode functionality
-// Need to be prompted to add more blog entries
-//Need an array of blog entries containing all Blog form fields
-//Need link to dveloper's portfolio
-//need functionality to be able to take a closer look at blog content and be presented with form headers
-
-
-uponSubmit.addEventListener('click', function (event) {
+function uponSubmit(event) {
     event.preventDefault();
+    
+    const username = document.getElementById('username').value.trim();
+    const title = document.getElementById('title').value.trim();
+    const content = document.getElementById('content').value.trim();
+
+    console.log( username, title, content );
 
     const blogForm = {
-        username: username.value,
-        title: title.value,
-        content: content.value.trim(),
+        username: username,
+        title: title,
+        content: content,
     };
 
 
     localStorage.setItem('blogForm', JSON.stringify(blogForm));
-    renderMessage();
-});
-
-function renderBlogForm() {
-    // Use JSON.parse() to convert text to JavaScript object
-    // Check if data is returned, if not exit out of the function
-    if (blogForm) {
-        document.getElementById('saved-username').innerHTML = username.value;
-        document.getElementById('saved-title').innerHTML = title.value;
-        document.getElementById('saved-content').innerHTML = content.value;
-    }
-}
-
-uponSubmit.addEventListener('click', function (event) {
-    event.preventDefault();
-    blogForm();
-    renderBlogForm();
-});
-
-function init() {
-    renderBlogForm();
-}
-init();
+  //  renderMessage();
+};
 
 
-// function renderMessage() {
-//   const lastGrade = JSON.parse(localStorage.getItem('blogForm'));
-//   if (lastGrade !== null) {
-// document.querySelector('.message').textContent =
-//   lastGrade.student + ` received a/an ${lastGrade.grade}`;
-//   }
-// }
+submitEl.addEventListener("click",uponSubmit );
+
+
+
